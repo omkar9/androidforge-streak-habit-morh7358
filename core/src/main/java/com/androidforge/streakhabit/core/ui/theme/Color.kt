db48042f81d486eca63f50d236fb334b4700031f
omkar9 <omkar946@gmail.com>
2026-05-04 23:01:04 +0530

    feat: add Color.kt

---
+++ core/src/main/java/com/androidforge/streakhabit/core/ui/theme/Color.kt
@@ -0,0 +1,30 @@
+package com.androidforge.streakhabit.core.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Neo-Brutalist Palette
+val Primary = Color(0xFF4A6D7C)
+val PrimaryVariant = Color(0xFF2C475A)
+val Secondary = Color(0xFFF2A30F)
+val Background = Color(0xFF1A1A1A)
+val Surface = Color(0xFF2B2B2B)
+val SurfaceVariant = Color(0xFF424242)
+val OnPrimary = Color(0xFFFFFFFF)
+val OnBackground = Color(0xFFE0E0E0)
+val OnSurface = Color(0xFFE0E0E0)
+val Success = Color(0xFF6BBF59)
+val Error = Color(0xFFD63F3F)
+val Warning = Color(0xFFF7C548)
+
+val LightPrimary = Color(0xFF4A6D7C)
+val LightPrimaryVariant = Color(0xFF2C475A)
+val LightSecondary = Color(0xFFF2A30F)
+val LightBackground = Color(0xFFF0F0F0)
+val LightSurface = Color(0xFFFFFFFF)
+val LightSurfaceVariant = Color(0xFFE0E0E0)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightOnBackground = Color(0xFF1A1A1A)
+val LightOnSurface = Color(0xFF1A1A1A)
+val LightSuccess = Color(0xFF6BBF59)
+val LightError = Color(0xFFD63F3F)
+val LightWarning = Color(0xFFF7C548)
